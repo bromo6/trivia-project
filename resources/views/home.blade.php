07a0996555d4cde192ec715a0ee04864d48df470
Unknown <brandonromo130@yahoo.com>
2018-03-19 16:28:35 -0500

    updated jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,20 @@
 <div id="homeJumbo" class="container-fluid">
   <div class="row">
       <div class="col">
-          <div class="row">
-            <div class="col-6">
-              <p>Hello world!</p>
-            </dvi>
+          <div class="row my-5 mx-5">
+            <div class="col-2">
+              <img class="rounded-circle emptyPro" height="100" width="100" src="images/empty-profile.png"/>
+            </div>
             <div class="col-4">
-              <img src="https://vignette.wikia.nocookie.net/boomerang-from-cartoon-network/images/d/d7/Daffy_Duck.png/revision/latest?cb=20161123215131" />
+              <h2>Hello world!</h2>
+            </div>
+            <div class="col-4">
+              <img height="400" src="https://vignette.wikia.nocookie.net/boomerang-from-cartoon-network/images/d/d7/Daffy_Duck.png/revision/latest?cb=20161123215131" />
             </div>
             <div class="col-2">
             </div>
           </div>
-
-          </div>
       </div>
-  </div>
+   </div>
 </div>
 @endsection
