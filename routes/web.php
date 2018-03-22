ed857895fb1928659534f06b5bc5b2047cc2d0b8
Unknown <javier.duarte12@yahoo.com>
2018-03-22 14:09:19 -0500

    added backend

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/questionaire', 'HomeController@searchQuestion');
+
+
+Route::get('settings', function () {
+    return view('settings');
+});
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index');
