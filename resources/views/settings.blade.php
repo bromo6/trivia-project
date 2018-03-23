14d79baef55497a25435319542621d032222678d
Unknown <brandonromo130@yahoo.com>
2018-03-22 23:15:09 -0500

    updated settings css

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,51 +1,51 @@
 @extends('master')
 @section('content')
 
-
-<div class="container homeBod">
-  <div class="row">
+<div class="container-fluid settingBack">
+  <div class="container settingBod">
+    <div class="row">
+        <div class="col">
+          <h1 class="text-center titleSet">Settings</h1>
+        </div>
+    </div>
+    <div class="row">
       <div class="col">
-        <h1 class="text-center">Settings</h1>
+        <p id="gamerTag"input="text">Username 12</p>
       </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      <p id="gamerTag"input="text">Username 12</p>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-6 newStuff">
-      <h2 id="newGame">Login Info</h2>
+    <div class="row">
+      <div class="col-6 ">
+        <h2 id="newGame">Login Info</h2>
 
-      <form>
-        First Name </br>
-        <input type="text" name="username" class="settingsPro"><br>
-        Last Name </br>
-        <input type="text" name="username" class="settingsPro"><br>
-        Username </br>
-        <input type="text" name="username" class="settingsPro"><br>
-        Email </br>
-        <input type="text" name="username" class="settingsPro"><br>
-        Password:<br>
-        <input type="password" name="psw" class="settingsPro">
-      </form>
-<br />
-
-      <h2 id="newGame1">Password Reset</h2>
-      <form>
-         New Password:<br />
-        <input type="password" name="psw" class="settingsPro"><br>
-        User password:<br>
-        <input type="password" name="psw" class="settingsPro"></br>
-        <button type="btn" class="btn btn-outline-secondary settingSubmit">Submit</button>
-      </form>
-
-
+        <form>
+          First Name </br>
+          <span class="settingsPro"></span>
+          <!-- <input type="text" name="username" class="settingsPro"><br> -->
+          Last Name </br>
+          <span class="settingsPro"></span>
+          <!-- <input type="text" name="username" class="settingsPro"><br> -->
+          Email </br>
+          <span class="settingsPro"></span>
+          <!-- <input type="text" name="username" class="settingsPro"><br> -->
+        </form>
+        <br />
+        <h2 id="newGame1">Password Reset</h2>
+        <form>
+          Current password:<br>
+          <input type="password" name="psw" class="settingsPro"></br>
+           New Password:<br />
+          <input type="password" name="psw" class="settingsPro"><br>
+          Confirm Password:<br />
+         <input type="password" name="psw" class="settingsPro"><br>
+          <button type="btn" class="btn btn-outline-dark settingSubmit">Submit</button>
+        </form>
+      </div>
+      <div class="col-6">
+        <img src="images/setPic2.png" />
+      </div>
     </div>
-
   </div>
 </div>
-
 
 
 @endsection
